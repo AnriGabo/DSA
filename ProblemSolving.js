208c00d16931222bd2456548385ecfeccb78a7e3
Anri <@anrigabaidze44@gmail.com>
2025-06-10 15:56:07 +0400

    integerboolean best practise

--- ProblemSolving.js
+++ ProblemSolving.js
@@ -5,12 +5,7 @@
 // ->input : 1 0 ების ნაკრები, 1 = true, 0 = false;
 
 function integerBoolean(integer) {
-  const modifyNum = [integer].join("").split("");
-  const mapingElement = modifyNum.map((Item) => Number(Item));
-  const compare = mapingElement.map((Itemiko) =>
-    Itemiko === 1 ? true : false
-  );
-  return compare;
+  return integer.split("").map((Item) => Number(Item) === 1 ? true : false)
 }
 
 console.log(integerBoolean("1010101010101"));
