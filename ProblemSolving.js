52fbfb08c6f57768d7b057f6ac876326e78f5925
Anri <@anrigabaidze44@gmail.com>
2025-06-12 00:34:57 +0400

    clone Array

--- ProblemSolving.js
+++ ProblemSolving.js
@@ -2,21 +2,11 @@
 // processing
 // Output --> string
 
+// input array, output: dublicate array
 
-
-// Create a function that takes an array of numbers and returns the second largest number.
-
-
-// -->numbers of array s
-// processing -> sort, slice
-
-const secondLargestNum = (num ) => {
-    // long way 
-    const sortingArray = num.sort((a,b) => b - a);
-    const uniqueElement = [...new Set(sortingArray)][1]
-    return uniqueElement
-    
+const clone = (arg) => {
+    const cloning = [...arg, ...arg]
+    return cloning;
 }
-console.log(secondLargestNum([20,343,324,2,22,2323,2,3,2,30]))
-// --> output 343
-// -->output: second largest number
+
+console.log(clone([1,2,3]))
