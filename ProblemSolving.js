b84b4663e2af499935932d1e0753e943f499e1b6
Anri <@anrigabaidze44@gmail.com>
2025-06-11 17:07:26 +0400

    find the second largest number

--- ProblemSolving.js
+++ ProblemSolving.js
@@ -2,21 +2,21 @@
 // processing
 // Output --> string
 
-// Write a function that converts an object into an array of keys and values.
 
-// -->Input Object
-// Object.keys, Object.values,Object.entries, Object.fromEntries,
 
-const arrayToObject = (arg) => {
-  return Object.fromEntries(arg)
-};
+// Create a function that takes an array of numbers and returns the second largest number.
 
-console.log(
-  arrayToObject([
-    ["anri", "gabo"],
-    ["anriko", "gabo"],
-    ["daviti","daviti"]
-  ])
-);
 
-//-->  Output [ [] [] [ ]] key valueდალაგეუბლი,მასივის სახით
+// -->numbers of array s
+// processing -> sort, slice
+
+const secondLargestNum = (num ) => {
+    // long way 
+    const sortingArray = num.sort((a,b) => b - a);
+    const uniqueElement = [...new Set(sortingArray)][1]
+    return uniqueElement
+    
+}
+console.log(secondLargestNum([20,343,324,2,22,2323,2,3,2,30]))
+// --> output 343
+// -->output: second largest number
