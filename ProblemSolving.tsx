de280922d26c050d040d4259e5dedf6e96dd2dd4
Anri <@anrigabaidze44@gmail.com>
2025-06-12 17:23:26 +0400

    testjackpot

---
+++ ProblemSolving.tsx
@@ -0,0 +1,12 @@
+// Input --> string and number
+// processing
+// Output --> string
+
+
+// Arrays:
+
+let mixedType: (string[] | number[])[] = [
+    [1,2,3,4],
+    ["anriko","gabski"],
+    [3,4,5,6]
+]
