2e4d69072a5031ba5e7d4e8186cf9d7ebb430b98
Anri <@anrigabaidze44@gmail.com>
2025-06-12 14:20:48 +0400

    check the Array

--- ProblemSolving.js
+++ ProblemSolving.js
@@ -2,11 +2,27 @@
 // processing
 // Output --> string
 
-// input array, output: dublicate array
+let customersArray = [
+  "Custy Stomer",
+  "C. Oostomar",
+  "C.U.S. Tomer",
+  3432434,
+  "Custo Mer",
+  "Custopher Ustomer",
+  3432435,
+  "Kasti Yastimeur",
+];
 
-const clone = (arg) => {
-    const cloning = [...arg, ...arg]
-    return cloning;
+// -->Input ში პარამეტრი ღებულობს ამ მასივს
+
+//-->Ouput -->ნებისმიერი ელემენტი რომელიც არ იქენაბ სტრინგი ეწოდოს el და დალოგოს ფუნქციამ რომ ეს el უნდა იყოს სტრინგ
+
+function checkCustomArray() {
+  for (var i = 0; i < customersArray.length; i++) {
+    if (typeof customersArray[i] != "string") {
+      console.log(`type error ${el} should be a string`);
+    }
+  }
 }
 
-console.log(clone([1,2,3]))
+console.log(checkCustomArray(customersArray));
