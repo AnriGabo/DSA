f24f9305e82c8482201119475e78204df7424fed
Anri <@anrigabaidze44@gmail.com>
2025-06-13 18:07:28 +0400

    countVowels

--- ProblemSolving2.js
+++ ProblemSolving2.js
@@ -1,20 +1,22 @@
-// Hitting the jackpot
+// count of vowels
 
-// შექმენით ფუნქცია რომელიც იღებს მასივს
+// input string
+// a, e, i, o, u is vowel
+// function vowelsCharachter(vowel) {
+//   const vowCharachter = vowel.split("");
+//   const filterElement = vowCharachter.filter(
+//     (item) => item === "a" || item === "e" || item === "i" || item === "o" || item === "u"
+//   );
 
-// Total Volume of All Boxes;
+//   return filterElement.length;
+// }
 
+// console.log(vowelsCharachter("Palm"));
+// // output რამდენ ხმოვანს შეიცავს ჩვენი სიტყვა და დააბრუნოს ლენგსი
 
-// Create function that takes 2 arguments
-//Input --> 2 String argument, first string is single charachter
+const countVowels = (str) => {
+  const vowels = "aeiou";
+  return str.toLowerCase().split("").filter((char) => vowels.includes(char)).length
+};
 
-// processing --> includes, first index of, last index of,
-
-function charCount(char,word){
-    const SplitTheWord = word.split("");
-    return SplitTheWord.filter((item) => item === char).length;
-}
-
-console.log(charCount("b","big fat bubble"))
-
-// Output რამდენჯერ განმეორდა პირველი სტრინგი მეორესტრინგში
+console.log(countVowels("Hello"))
