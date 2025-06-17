c6571537b8c7909969fb5387447e08e0cdfccefe
Anri <@anrigabaidze44@gmail.com>
2025-06-17 22:40:22 +0400

    convertBinary

--- ProblemSolving2.js
+++ ProblemSolving2.js
@@ -1,9 +1,14 @@
-// -->input: String word
-// processing: reverse
-// Output: reverse the string word by word, first word will be the last, and last will be first
+// Transforming Words into Binary Strings
 
-function reverseWord(opositeWord) {
-  return opositeWord.split(" ").reverse().join("-")
+// input -- string word
+// output:binary string
+
+// letters from [a,m] equal to 0, and letters from [n,z] to 1;
+
+
+function convertBinary(str){
+    const AtoM = "ABCDEFGHIJKLM".toLowerCase();
+    return str.split("").map((Item) => AtoM.includes(Item) ? 0 : 1).join("")
 }
 
-console.log(reverseWord("anri gabaidze"));
+console.log(convertBinary("moon"))
