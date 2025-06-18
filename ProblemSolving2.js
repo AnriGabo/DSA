b523220bd4ddf3eb1f7b145fe3d7b7f040d43425
Anri <@anrigabaidze44@gmail.com>
2025-06-19 01:46:16 +0400

    updated version of highlow we using built in method math max and math min with spread operator

--- ProblemSolving2.js
+++ ProblemSolving2.js
@@ -6,13 +6,15 @@
 
 // -- იმ შემთხვევაში რეალურად თუ იქნება 1 ციფრი, დააბრუნებს 1ს ორად ანუ ეგ იქნება დიდიც და პატარაც
 
+function highLow(strNum) {
+  const convertToNumber = strNum.split(" ").map(Number);
 
-function highLow(strNum){
-    const convertToArrayString = strNum.split(" ")
-    const highestNum = convertToArrayString.sort((a,b) => b -a ).slice(0,1);
-    const lowestNum = convertToArrayString.sort((a,b) => a - b).slice(0,1);
-    return `"${highestNum} ${lowestNum}"`
+  const highestNum = Math.max(...convertToNumber);
+  const lowestNum = Math.min(...convertToNumber);
 
+  return `"${highestNum} ${lowestNum}"`;
 }
 
-console.log(highLow("10 3204 234 2340234324 23402304 30"))
+// Math max, math min
+
+console.log(highLow("1 2 3 4 5"));
