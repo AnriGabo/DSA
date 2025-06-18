44e298ad25cca031fa9a6f4ad6b0a71cb0a24e5e
Anri <@anrigabaidze44@gmail.com>
2025-06-19 01:40:43 +0400

    highlow

--- ProblemSolving2.js
+++ ProblemSolving2.js
@@ -1,6 +1,18 @@
+// Return the Highest and Lowest Numbers
 
-function convertASCII(str) {
-  return str.split("").reduce((acc, char) => acc + char.charCodeAt(0), 0);
+// Input String სივრცეებით გამოტოვებული ციფრები
+
+// Output String ყველაზე დიდი ციფრი პირველი, და მერე ყველაზე დაბალი ციფრი მეორე
+
+// -- იმ შემთხვევაში რეალურად თუ იქნება 1 ციფრი, დააბრუნებს 1ს ორად ანუ ეგ იქნება დიდიც და პატარაც
+
+
+function highLow(strNum){
+    const convertToArrayString = strNum.split(" ")
+    const highestNum = convertToArrayString.sort((a,b) => b -a ).slice(0,1);
+    const lowestNum = convertToArrayString.sort((a,b) => a - b).slice(0,1);
+    return `"${highestNum} ${lowestNum}"`
+
 }
 
-console.log(convertASCII("anri"));
+console.log(highLow("10 3204 234 2340234324 23402304 30"))
