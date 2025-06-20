d8be2c686a34edfd2b726dd802baf3ed20d178d1
Anri <@anrigabaidze44@gmail.com>
2025-06-20 13:04:16 +0400

    filtered

--- ProblemSolving2.js
+++ ProblemSolving2.js
@@ -1,13 +1,28 @@
-// Create a function that takes an integer n and reverses it.
+function filteredArray(arr, elem) {
+  let newArr = [];
+  for(var i = 0; i < arr.length; i++){
 
-// შექმენით ფუნქცია, რომელიც იღებს სტრინგის მთელ რიცხვს და აბრუნებს მას, reverse, reduce : )
-// Math.abs  negative convert to positive
-
-function reverseTheNumber(reverse) {
-  return Math.abs(reverse)
-    .toString()
-    .split("")
-    .reduce((acc, char) => char + acc, 0);
+    // თუ მასივი არ შეიცავს ელემენტს
+    if(!arr[i].includes(elem)){
+        newArr.push(arr[i])
+    }
+  }
+  return newArr;
 }
 
-console.log(reverseTheNumber("-23432423"));
+console.log(
+  filteredArray(
+    // SubArray, ..[arr[i]]
+    // მასივი რომელიც შეიცავს 3 ს არდაააბრუნოს, ამ შემთხვევაში ცარიელი მასივი უნდა დაბრუნდეს
+    [
+      [1,1,1],
+      [1, 6, 3],
+      [3, 13, 26],
+      [19, 3, 9],
+    ],
+    1
+  )
+);
+
+// Includes
+// თუ ეს მასივი არ შეიცავს 3-ს,დააბრუნსო ელემენტები
