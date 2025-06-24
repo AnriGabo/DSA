a1cb82bbc078d5d4f461e4e207a44ff629a9b33a
Anri <@anrigabaidze44@gmail.com>
2025-06-25 01:31:52 +0400

    SumOfOddAndEvenNumber

--- ProblemSolving2.js
+++ ProblemSolving2.js
@@ -1,22 +1,14 @@
-// Create a function that takes a string and returns a new string with its first and last characters swapped,
+// Input array of number
+// Ouput array with 2 elements პირველი ელემენტი არის ლუწი ციფრების ჯამი, მეორე ელემენტი არის კენტი ციფრების ჯამი
 
-// Input შექმენით ფუნქცია რომელიც იღებს სტრინგს
-
-// Output და აბრუნებს ახალ სტრინგს მის პირველ და ბოლო ასოების გაცვლით
-// თუ წერია რომ Anri -> inrA, და ასე შემდეგ
-
-// ეს პირობა უნდა გავითვალისწინოთ აგერ
-
-// If the length of the string is less than two, return "Incompatible.".
-// If the argument is not a string, return "Incompatible.".
-
-function flipEndChars(word) {
-  // სამივე If-ს შეადარებს და თუ სამივე True იყო დააბრუნებს ბოლო Return-ს
-  if(word.length <= 2) return "Incompatible, To Short";
-  if(typeof word != "string") return "Incompatible, Wrong Type";
-  if(word[0] === word[word.length - 1]) return "Incompatible, Two's Pair";
-
-  return word.split("").reduce((acc,char) => char + acc,  "")
+function SumOfOddAndEvenNum(num) {
+  const odd = num
+    .filter((Item) => Item % 2 === 0)
+    .reduce((acc, char) => acc + char, 0);
+  const even = num
+    .filter((Item) => Item % 2 === 1)
+    .reduce((acc, char) => acc + char, 0);
+  return `${odd} ${even}`.split(" ").map(Number);
 }
 
-console.log(flipEndChars("cat, mouse, dog."));
+console.log(SumOfOddAndEvenNum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
