479ef126db28ccdcce7851adfbc6197d69065a79
Anri <@anrigabaidze44@gmail.com>
2025-06-25 01:37:21 +0400

    SumOfOddAndEvenNumber update version

--- ProblemSolving2.js
+++ ProblemSolving2.js
@@ -2,13 +2,13 @@
 // Ouput array with 2 elements პირველი ელემენტი არის ლუწი ციფრების ჯამი, მეორე ელემენტი არის კენტი ციფრების ჯამი
 
 function SumOfOddAndEvenNum(num) {
-  const odd = num
+  const evenSum = num
     .filter((Item) => Item % 2 === 0)
     .reduce((acc, char) => acc + char, 0);
-  const even = num
-    .filter((Item) => Item % 2 === 1)
+  const oddSum = num
+    .filter((Item) => Math.abs(Item % 2) === 1)
     .reduce((acc, char) => acc + char, 0);
-  return `${odd} ${even}`.split(" ").map(Number);
+  return [evenSum, oddSum];
 }
 
 console.log(SumOfOddAndEvenNum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
